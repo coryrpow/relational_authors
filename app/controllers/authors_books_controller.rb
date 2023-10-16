0c6bb24e8bcd77677180f759fec34be3ea2c724f
Cory Powell <coryrpowell@gmail.com>
2023-10-16 17:24:25 -0400

    created the new view for the authors_books page with a form to create a new book and added the create method in the controller

--- app/controllers/authors_books_controller.rb
+++ app/controllers/authors_books_controller.rb
@@ -5,5 +5,23 @@ class AuthorsBooksController < ApplicationController
     @books = @author.books
   end
 
+  def new
+    @author = Author.find(params[:id])
+
+    @books = @author.books
+  end
+
+  def create
+    author = Author.find(params[:id])
+    book = author.books.new({
+      title: params[:title],
+      published: params[:published],
+      in_print: (params[:in_print] || false),
+      translated_english: (params[:translated_english] || false)
+    })
+    book.save 
+    redirect_to "/authors/#{author.id}/books"
+  end
+
 
 end
